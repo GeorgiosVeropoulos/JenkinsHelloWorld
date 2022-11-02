974f0752061cd3d9db682a3b7f1d40644be2a68b
GeorgiosVeropoulos <40268741+GeorgiosVeropoulos@users.noreply.github.com>
2022-11-02 14:25:55 +0100

    edited in github

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("New Changes");
+        System.out.println("I edited this in GitHub directly");
 
 
 

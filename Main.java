31b1ee0f91e79b202b1b987e1c1347a8b7262518
GeorgiosVeropoulos <40268741+GeorgiosVeropoulos@users.noreply.github.com>
2022-11-02 14:12:29 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World of Jenkins");
+        System.out.println("Made some changes");
     }
 }
 
